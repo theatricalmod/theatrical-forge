e227a07bfda82973124cd25cfe5db02588cdfb9b
James <615283@users.noreply.github.com>
2018-08-21 22:15:07 +0100

    attempt to fix

--- src/main/java/com/georlegacy/general/theatrical/items/fixtureattr/gel/Gel.java
+++ src/main/java/com/georlegacy/general/theatrical/items/fixtureattr/gel/Gel.java
@@ -1,31 +1,30 @@
 package com.georlegacy.general.theatrical.items.fixtureattr.gel;
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
 
 public class Gel extends Item {
 
-    private final NonNullList<ItemStack> subItems;
+    @Override
+    public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items) {
+        for (GelType gelType : GelType.values()) {
+            items.add(new ItemStack(
+                    this.setUnlocalizedName(gelType.getName() + " Gel (" + gelType.getId() + ")"),
+                    1,
+                    gelType.getId()
+            ));
+        }
+    }
 
     public Gel() {
-        subItems = NonNullList.create();
         this
                 .setRegistryName("theatrical", "gel")
                 .setUnlocalizedName("Unknown Gel")
                 .setMaxStackSize(64)
                 .setMaxDamage(0)
                 .setHasSubtypes(true);
-
-        this.getSubItems(null, subItems);
-
-        for (GelType gelType : GelType.values()) {
-            subItems.add(new ItemStack(
-                    this.setUnlocalizedName(gelType.getName() + " Gel (" + gelType.getId() + ")"),
-                    1,
-                    gelType.getId()
-            ));
-        }
     }
 
 
