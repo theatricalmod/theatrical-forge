7c2ab703776965590e0d799388934a1ed76884bb
James <615283@users.noreply.github.com>
2018-08-22 10:53:33 +0100

    gel texture assets

--- src/main/java/com/georlegacy/general/theatrical/tabs/CreativeTabs.java
+++
@@ -1,7 +0,0 @@
-package com.georlegacy.general.theatrical.tabs;
-
-public class CreativeTabs {
-
-  public static final TheatricalMainTab mainTab = new TheatricalMainTab();
-
-}
---
+++ src/main/java/com/georlegacy/general/theatrical/tabs/base/CreativeTabs.java
@@ -0,0 +1,9 @@
+package com.georlegacy.general.theatrical.tabs.base;
+
+import com.georlegacy.general.theatrical.tabs.TheatricalMainTab;
+
+public class CreativeTabs {
+
+  public static final TheatricalMainTab mainTab = new TheatricalMainTab();
+
+}
